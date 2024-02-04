f1b631b5e784da5a21bbae0d5e9146bd26d95053
SIRFozzieSEA <github@codef.com>
2024-02-04 09:52:54 -0800

    Adding tap detectors to stop/start

--- app/src/main/java/com/codef/digitalfan/MainActivity.kt
+++ app/src/main/java/com/codef/digitalfan/MainActivity.kt
@@ -2,21 +2,29 @@ package com.codef.digitalfan
 
 import android.net.Uri
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.annotation.OptIn
+import androidx.core.view.GestureDetectorCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
 
-class MainActivity : ComponentActivity() {
+class MainActivity : ComponentActivity(),
+    GestureDetector.OnDoubleTapListener, GestureDetector.OnGestureListener {
 
     private lateinit var exoPlayer: ExoPlayer
+    private lateinit var gestureDetector: GestureDetectorCompat
 
     @OptIn(UnstableApi::class) override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        gestureDetector = GestureDetectorCompat(this, this)
+        gestureDetector.setOnDoubleTapListener(this)
 
         val mediaItemClipped =
             MediaItem.Builder()
@@ -35,13 +43,69 @@ class MainActivity : ComponentActivity() {
         exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
         exoPlayer.setHandleAudioBecomingNoisy(true)
         exoPlayer.prepare()
-        exoPlayer.play()
 
     }
 
     override fun onDestroy() {
         super.onDestroy()
         exoPlayer.release()
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+    }
+
+    override fun onFling(
+        event1: MotionEvent?,
+        event2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onScroll(
+        event1: MotionEvent?,
+        event2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        return true
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        if (exoPlayer.isPlaying) {
+            exoPlayer.pause()
+        } else {
+            exoPlayer.play()
+        }
+        return true
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean {
+        return true
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+        return true
     }
 
 }

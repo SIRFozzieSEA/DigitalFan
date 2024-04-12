75baad2c0b5dc34971f771b4622b9dd1694753c6
SIRFozzieSEA <github@codef.com>
2024-04-11 19:42:49 -0700

    Removing log imports and lines

--- app/src/main/java/com/codef/digitalfan/DigitalFanAlarmReceiver.kt
+++ app/src/main/java/com/codef/digitalfan/DigitalFanAlarmReceiver.kt
@@ -3,14 +3,12 @@ package com.codef.digitalfan
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 
 class DigitalFanAlarmReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
 
         val exoPlayer = DigitalFanExoPlayerSingleton.getInstance(context)
-        Log.d("bobo", "KICKING OFF ALARM!")
         DigitalFanExoPlayerSingleton.setupExoPlayerAlarm(exoPlayer)
 
     }
--- app/src/main/java/com/codef/digitalfan/DigitalFanMainActivity.kt
+++ app/src/main/java/com/codef/digitalfan/DigitalFanMainActivity.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
